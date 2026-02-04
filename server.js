b74de734e719ebb4c1fb6ca138e46306c3530950
Sujal soni <106693181+Sujal8182@users.noreply.github.com>
2026-02-04 22:12:31 +0530

    Add production URL to CORS allowed origins

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 app.use(
   cors({
-    origin: ['http://localhost:5173',"http://localhost:5174"],
+    origin: ['http://localhost:5173',"http://localhost:5174", "https://airline-booking-backend-58y6.onrender.com"],
     credentials: true,               
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
